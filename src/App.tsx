cb97ec3b29482a4dfa79181674ebeb0372813350
Moritz Thüning <moritz.thuening@gmail.com>
2023-09-08 21:04:32 +0200

    first working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// @ts-nocheck
+
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 
+async function createPeerConnection() {
+  let iceServers = [
+    {
+      urls: 'stun:stun.l.google.com:19302',
+    },
+  ]
+  const peerConnection = new RTCPeerConnection({
+    sdpSemantics: 'unified-plan',
+    iceServers: iceServers,
+  })
+  const dataChannel = peerConnection.createDataChannel('datachannel')
+  const offer = await peerConnection.createOffer({
+    offerToReceiveVideo: true,
+  })
+  await peerConnection.setLocalDescription(offer)
+  if (peerConnection.iceGatheringState !== 'complete') {
+    await new Promise((resolve) => {
+      function checkState() {
+        if (peerConnection.iceGatheringState === 'complete') {
+          peerConnection.removeEventListener('icegatheringstatechange', checkState)
+          resolve()
+        }
+      }
+      peerConnection.addEventListener('icegatheringstatechange', checkState)
+    })
+  }
+  return { peerConnection: peerConnection, dataChannel: dataChannel }
+}
+
+function connectSignaler(openCallback, messageCallback, closeCallback) {
+  const socket = new WebSocket('ws://localhost:8080/connect')
+  socket.addEventListener('open', () => {
+    openCallback(socket)
+  })
+  socket.addEventListener('message', (event) => {
+    messageCallback(socket, JSON.parse(event.data))
+  })
+  socket.addEventListener('close', () => {
+    closeCallback(socket)
+  })
+  return socket
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [peerIds, setPeerIds] = useState([])
+  const [dataChannel, setDataChannel] = useState(null)
+  const signaler = useRef(null)
+  const peerConnection = useRef(null)
+  const refVideo = useRef<HTMLVideoElement>(null)
+  const initialized = useRef(false)
+
+  async function sendOffer(peerId) {
+    const response = await createPeerConnection()
+    response.dataChannel.addEventListener('message', (e) => console.log(e.data))
+    response.peerConnection.addEventListener('track', ({ track }) => {
+      refVideo.current.srcObject = new MediaStream([track])
+    })
+    peerConnection.current = response.peerConnection
+    setDataChannel(response.dataChannel)
+    const offer = peerConnection.current.localDescription
+    signaler.current.send(
+      JSON.stringify({
+        to: peerId,
+        message: {
+          sdp: offer.sdp,
+          type: offer.type,
+        },
+      })
+    )
+  }
+
+  function handleMessage(socket, message) {
+    switch (message.type) {
+      case 'peers':
+        setPeerIds(message.peerIds)
+        if (message.peerIds.length > 0) {
+          sendOffer(message.peerIds[0])
+        }
+        break
+      case 'rtc':
+        peerConnection.current.setRemoteDescription(message.message)
+        break
+    }
+  }
+
+  useEffect(() => {
+    if (initialized.current) return
+    initialized.current = true
+    signaler.current = connectSignaler(
+      () => console.log('open'),
+      handleMessage,
+      console.log('closed')
+    )
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <video ref={refVideo} autoPlay muted draggable="false" />
     </>
   )
 }
